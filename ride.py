e8907259ccc22c6aa953269cad24fa8119619634
glouglou000 <61392905+glouglou000@users.noreply.github.com>
2020-03-08 22:55:43 +0100

    Update ride.py

--- ride.py
+++ ride.py
@@ -19,6 +19,10 @@ from scipy.spatial import distance as dist
 
 
 
+from figure.wrinkle.ride_utils import raising_part
+from figure.wrinkle.ride_utils import wrinkle_function
+from figure.wrinkle.ride_utils import skin_detector
+from figure.wrinkle.ride_utils import raising
 
 
 
@@ -74,51 +78,7 @@ face_division = 1.650000000000001
 
 
 
-
-def skin_detector(frame):
-
-
-    min_YCrCb = np.array([0,140,85],np.uint8)
-    max_YCrCb = np.array([240,180,130],np.uint8)
-    imageYCrCb = cv2.cvtColor(frame, cv2.COLOR_BGR2YCR_CB)
-    skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
-    skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 2)
-    skinMask = cv2.morphologyEx(skinMask, cv2.MORPH_CLOSE, kernel)
-
-    skinYCrCb = cv2.bitwise_and(frame, frame, mask = skinMask)
-
-    return skinYCrCb
-
-
-
-
-
-def wrinkle_function(head_box_head, data_points, data_feature, landmarks,
-                     frame_head, threshold, mode1, mode2, number):
-
-    _, _, width_head, height_head = head_box_head
-
-    adding, points = data_points
-
-    convexPoints = recuperate_coordinates(points, adding, landmarks, frame_head, mode1)
-    crop_threhsold, crop_frame, box_crop = masks_from_convex(convexPoints, threshold, frame_head)
-
-
-    maxContour, minContour, minLength, maxLength, minWidth, maxWidth = data_feature
-
-    localisation_wrinkle(crop_threhsold, box_crop,
-                         minContour, maxContour, minLength,
-                         maxLength, minWidth, maxWidth, mode2, crop_frame, number)
-
-
-
-
-
-        
-
-def a(zero):
+def wrinkle_crow_feet(zero):
 
     #crow feet 
     data_points_crow_right = ( ( (0, 0), (0, 0), (0, 0) ), ( (36, 37), (17, 37), (36, 31), (0, 17) ) )
@@ -141,7 +101,7 @@ def a(zero):
     t2.join()
     
 
-def b(zero, width_head, height_head):
+def wrinkle_beetween_eye(zero, width_head, height_head):
 
     #Beetween eyes.
     adding_height = int(height_head * 0.09)   #5 de 74
@@ -159,7 +119,7 @@ def b(zero, width_head, height_head):
 
 
 
-def c(zero, width_head):
+def wrinkle_side_mouth(zero, width_head):
 
     #side mouth
     add1 = int(width_head * 0.12) # 10 de 87 x1
@@ -183,7 +143,9 @@ def c(zero, width_head):
     t1.join()
     t2.join()
 
-def d(zero, height_head):
+
+
+def wrinkle_under_eye(zero, height_head):
     add_height = int(height_head * 0.1) #8 de 85
 
     #under
@@ -213,34 +175,6 @@ def d(zero, height_head):
 
 
 
-#------- Raise anatomy --------
-
-def raising_part(landmarks_head, points_list, picture, head_box_head, adding_height, add_width):
-    """Put white on region interest on a gray picture"""
-
-    #Add height px of our y points.
-    width, height = head_box_head[2:]
-    add_height_to_points = int(height * adding_height)
-    add_width_to_points = int(height * add_width)
-    
-    #Recuperate landmarks 1:-1
-    region = [(landmarks_head.part(n).x, landmarks_head.part(n).y - add_height_to_points)
-              for n in points_list[1: -1]]
-
-    #First and last landmark (for hide on eyes)
-    region1 = [(landmarks_head.part(points_list[0]).x + add_width_to_points,
-                landmarks_head.part(points_list[0]).y)]
-    region2 = [(landmarks_head.part(points_list[-1]).x + add_width_to_points,
-                landmarks_head.part(points_list[-1]).y)]
-
-    #Make one list
-    region1 += region
-    region1 += region2
-
-    #Transfor points into array
-    region = np.array(region1)
-    #Fill the region in white color on a gray picture
-    cv2.fillPoly(picture, [region], (255, 255, 255))
 
 
 
@@ -255,10 +189,9 @@ def raising_part(landmarks_head, points_list, picture, head_box_head, adding_hei
 
 
 
-#------- Raise -----------
 
-def raising_anatomy(landmarks_head, threshold, head_box_head, liste, adding_height, add_width):
-    raising_part(landmarks_head, liste, threshold, head_box_head, adding_height, add_width)
+
+
 
 
 #------- Filters -----------
@@ -344,75 +277,30 @@ while True:
         filter2.join()
 
 
-        #----------Raising part------------
 
-        raising_on_eyes1 = TH(target=raising_anatomy(landmarks_head, threshold,
-                                                               head_box_head, [17, 18, 19, 20, 21],
-                                                                0.055, 0))# 5 91
-
-        raising_on_eyes2 = TH(target=raising_anatomy(landmarks_head, threshold,
-                                                               head_box_head, [22, 23, 24, 25, 26],
-                                                                0.055, 0))
-
-        raising_mouse = TH(target=raising_anatomy(landmarks_head, threshold, head_box_head,
-                                                                [31, 48, 57, 54, 35],
-                                                                0, 0))
-
-        raising_eye1 = TH(target=raising_anatomy(landmarks_head, threshold1, head_box_head,
-                                                                [36, 37, 38, 39, 40, 41],
-                                                                0, 0))
-
-        raising_eye2 = TH(target=raising_anatomy(landmarks_head, threshold1, head_box_head,
-                                                                [42, 43, 44, 45, 46, 47],
-                                                                0, -0.5))
-
-        raising_nose = TH(target=raising_anatomy(landmarks_head, threshold, head_box_head,
-                                                                [31, 32, 33, 34, 35, 28],
-                                                                0, 0.5))
-
-        raising_eye3 = TH(target=raising_anatomy(landmarks_head, threshold, head_box_head,
-                                                                [36, 37, 38, 39, 40, 41],
-                                                                0, 0))
-
-        raising_eye4 = TH(target=raising_anatomy(landmarks_head, threshold, head_box_head,
-                                                                [42, 43, 44, 45, 46, 47],
-                                                                0, -0.5))
-
-        raising_on_eyes1.start()
-        raising_on_eyes2.start()
-        raising_mouse.start()
-        raising_eye1.start()
-        raising_eye2.start()
-        raising_nose.start()
-
-
-        raising_on_eyes1.join()
-        raising_on_eyes2.join()
-        raising_mouse.join()
-        raising_eye1.join()
-        raising_eye2.join()
-        raising_nose.join()
+        #Raising part
+        raising(landmarks_head, threshold, threshold1, head_box_head)
 
 
         
         zero = 0
         _, _, width_head, height_head = head_box_head
 
-        aa = TH(target=a(zero))
-        bb = TH(target=b(zero, width_head, height_head))
-        cc = TH(target=c(zero, width_head))
-        dd = TH(target=d(zero, height_head))
+##        th_wrinkle_crow = TH(target=wrinkle_crow_feet(zero))
+##        th_beetween     = TH(target=wrinkle_beetween_eye(zero, width_head, height_head))
+##        th_side_mouth   = TH(target=wrinkle_side_mouth(zero, width_head))
+        th_under_eyes   = TH(target=wrinkle_under_eye(zero, height_head))
 
 
-        aa.start()
-        bb.start()
-        cc.start()
-        dd.start()
+        #th_wrinkle_crow.start()
+        #th_beetween.start()
+        #th_side_mouth.start()
+        th_under_eyes.start()
 
-        aa.join()
-        bb.join()
-        cc.join()
-        dd.join()
+        #th_wrinkle_crow.join()
+        #th_beetween.join()
+        #th_side_mouth.join()
+        th_under_eyes.join()
 
 
 
